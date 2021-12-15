85d049889503536a64dd998c0f2975fa72a36bb9
pramod <kpramod8095@allstate.com>
2021-12-15 23:29:38 +0530

    new master

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,4 @@
-/**
- * Flatlogic Dashboards (https://flatlogic.com/admin-dashboards)
- *
- * Copyright © 2015-present Flatlogic, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
+
 
 import React from 'react';
 import PropTypes from 'prop-types';
@@ -27,13 +20,7 @@ class Footer extends React.Component {
     return (
       <footer className={cx(s.root, this.props.className)}>
         <div className={s.container}>
-          <span>© {new Date().getFullYear()} &nbsp;<a href="https://flatlogic.com" rel="noopener noreferrer" target="_blank">Flatlogic LLC</a> </span>
-          <span className={s.spacer}>·</span>
-          <Link to="/app/tos">Terms of Service</Link>
-          <span className={s.spacer}>·</span>
-          <Link to="/app/privacy">Privacy Policy</Link>
-          <span className={s.spacer}>·</span>
-          <Link to="/app/not-found">Support</Link>
+         
         </div>
       </footer>
     );
